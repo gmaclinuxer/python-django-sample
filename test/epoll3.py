25e528c1dcbabd2ff55a5aa875d1b2d8177bf764
hongsonggao <hongsonggao@tencent.com>
2017-01-03 19:19:06 +0800

    test epoll3

--- test/epoll3.py
+++ test/epoll3.py
@@ -3,14 +3,15 @@ import socket, select
 
 EOL1 = b'\n\n'
 EOL2 = b'\n\r\n'
+back_log = 1024
 response = b'HTTP/1.0 200 OK\r\nDate: Mon, 1 Jan 1996 01:01:01 GMT\r\n'
 response += b'Content-Type: text/plain\r\nContent-Length: 13\r\n\r\n'
 response += b'Hello, world!'
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind(('0.0.0.0', 8000))
-server_socket.listen(1)
+server_socket.bind(('0.0.0.0', 8888))
+server_socket.listen(back_log)
 # non-block mode
 server_socket.setblocking(0)
 
@@ -18,10 +19,20 @@ server_socket.setblocking(0)
 epoll = select.epoll()
 epoll.register(server_socket.fileno(), select.EPOLLIN)
 
+print ('''
+    epoll           : %s
+    server_socket   : %s
+    server          : %s
+    backlog         : %s
+''' % (epoll.fileno(), server_socket.fileno(), '%s:%s' % server_socket.getsockname(), 1024))
+
 try:
     cons, reqs, resps = {}, {}, {}
     while True:
-        events = epoll.poll(1)
+        events = epoll.poll(10)
+        if not events:
+            print('empty event: %s' % events)
+            continue
         for fileno, event in events:
             # client connect event
             if fileno == server_socket.fileno():
@@ -50,13 +61,18 @@ try:
                 if len(resps[fileno]) == 0:
                     # disable interest in further read or write events
                     epoll.modify(fileno, 0)
-                    cons[fileno].shutdown(socket.SHUT_RDWR)
+                    epoll.unregister(fileno)
+                    cons[fileno].close()
+                    del cons[fileno]
+                    # cons[fileno].shutdown(socket.SHUT_RDWR)
             elif event & select.EPOLLHUP:
                 # client shutdown connection first
                 epoll.unregister(fileno)
                 cons[fileno].close()
                 del cons[fileno]
+                del reqs[fileno]
 finally:
+    print reqs
     epoll.unregister(server_socket.fileno())
     epoll.close()
     server_socket.close()
--- test/epoll4.py
+++ test/epoll4.py
@@ -1,30 +1,36 @@
 # coding=utf-8
 from __future__ import print_function
+
 import socket, select, errno
 import datetime
 
+DEBUG = False
+back_log = 1024
 EOL1 = b'\n\n'
 EOL2 = b'\n\r\n'
-response = b'HTTP/1.0 200 OK\r\nDate: Mon, 1 Jan 1996 01:01:01 GMT\r\n'
+response = b''
+response += b'HTTP/1.0 200 OK\r\nDate: Mon, 1 Jan 1996 01:01:01 GMT\r\n'
 response += b'Content-Type: text/plain\r\nContent-Length: 13\r\n\r\n'
 response += b'Hello, world!'
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind(('0.0.0.0', 8888))
-server_socket.listen(1)
+server_socket.bind(('', 8888))
+server_socket.listen(back_log)
 # non-block mode
-server_socket.setblocking(0)
+server_socket.setblocking(False)
 
 # create epoll object
 epoll = select.epoll()
 epoll.register(server_socket.fileno(), select.EPOLLIN | select.EPOLLET)
 
-print ('''
+print('''
     epoll           : %s
     server_socket   : %s
     server          : %s
-''' % (epoll.fileno(), server_socket.fileno(), '%s:%s' % server_socket.getsockname()))
+    backlog         : %s
+    debug           : %s
+''' % (epoll.fileno(), server_socket.fileno(), '%s:%s' % server_socket.getsockname(), back_log, DEBUG))
 
 
 def trans_event(event_no):
@@ -42,8 +48,7 @@ try:
 except ImportError:
     import buildins as __build_in__
 
-DEBUG = True
-if DEBUG is False:
+if DEBUG is True:
     def print(*args, **kwargs):
         '''
         custom print
@@ -55,16 +60,18 @@ try:
     cons, reqs, resps = {}, {}, {}
     cnt = 0
     while True:
-        events = epoll.poll()
+        events = epoll.poll(10)
+        # print('------epoll.poll------\r\n')
         for fileno, event in events:
             # print('%s: fd: %s, event: %s' % (datetime.datetime.now(), fileno, trans_event(event)))
             # client connect event
             if fileno == server_socket.fileno():
                 try:
                     while True:
+                        # print('------accept entry------\r\n')
                         client_socket, address = server_socket.accept()
                         # non-block mode
-                        client_socket.setblocking(0)
+                        client_socket.setblocking(False)
                         # add client socket to epoll objects
                         epoll.register(client_socket.fileno(), select.EPOLLIN | select.EPOLLET)
                         # store client socket
@@ -72,14 +79,13 @@ try:
                         reqs[client_socket.fileno()] = b''
                         resps[client_socket.fileno()] = response
                 except socket.error as e:
-                    # print('%s: EPOLLIN-accept: %s, EAGAIN: %s, client: %s' % (datetime.datetime.now(), e.errno, errno.EAGAIN, client_socket.fileno()))
+                    print('%s: EPOLLIN-accept: %s, EAGAIN: %s, client: %s' % (
+                    datetime.datetime.now(), e.errno, errno.EAGAIN, client_socket.fileno()))
                     pass
             elif event & select.EPOLLIN:
                 try:
                     while True:
-                        recv_str = cons[fileno].recv(1024)
-                        # print recv_str, fileno
-                        reqs[fileno] += recv_str
+                        reqs[fileno] += cons[fileno].recv(1024)
                 except socket.error as e:
                     print('%s: EPOLLIN: %s, EAGAIN: %s' % (datetime.datetime.now(), e.errno, errno.EAGAIN))
                     pass
@@ -96,22 +102,31 @@ try:
                         bytes_written = cons[fileno].send(resps[fileno])
                         resps[fileno] = resps[fileno][bytes_written:]
                 except socket.error as e:
-                    # print('%s: EPOLLOUT: %s, EAGAIN: %s' % (datetime.datetime.now(), e.errno, errno.EAGAIN))
+                    print('%s: EPOLLOUT: %s, EAGAIN: %s' % (datetime.datetime.now(), e.errno, errno.EAGAIN))
                     pass
 
                 # finish write
                 if len(resps[fileno]) == 0:
                     # disable interest in further read or write events
+                    if fileno in reqs:
+                        del reqs[fileno]
+                    # server close connection
                     epoll.modify(fileno, select.EPOLLET)
+                    # epoll.unregister(fileno)
+                    # cons[fileno].close()
+                    # del cons[fileno]
                     cons[fileno].shutdown(socket.SHUT_RDWR)
 
             elif event & select.EPOLLHUP:
-                # client shutdown connection first
-                # print('%s: EPOLLHUP-%s' % (datetime.datetime.now(), fileno))
+                # client close connection
+                print('%s: EPOLLHUP-%s' % (datetime.datetime.now(), fileno))
                 epoll.unregister(fileno)
-                cons[fileno].close()
+                # cons[fileno].close()
                 del cons[fileno]
+                if fileno in reqs:
+                    del reqs[fileno]
 finally:
+    print(reqs)
     epoll.unregister(server_socket.fileno())
     epoll.close()
     server_socket.close()
