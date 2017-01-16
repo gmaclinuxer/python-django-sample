d56ca447c3037d5a76092a4395e77335bb37a148
gmaclinuxer <hongsonggao@tencent.com>
2017-01-17 01:12:01 +0800

    chart app

--- config/urls.py
+++ config/urls.py
@@ -4,8 +4,10 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('chat.urls')),
-    url(r'^chart/', include('chartapp.urls')),
+    # url(r'^', include('chat.urls')),
+    url(r'^chat/', include('chat.urls')),
+    # url(r'^chart/', include('chartapp.urls')),
+    url(r'^', include('chartapp.urls')),
     url(r'^fabric/', include('fabric_master.urls')),
     url(r'^explore/', include('explore.urls')),
 ]
